41e78e1fd7fab4d770839e1c4709f3c9bdff53e0
ManojKS457 <manojdab10@gmail.com>
2026-05-21 22:31:50 +0530

    Update add_expense.py

--- modules/add_expense.py
+++ modules/add_expense.py
@@ -1,69 +1,33 @@
 import streamlit as st
-import sqlite3
 
 def show_add_expense():
 
-    st.header("➕ Add Transaction")
+    st.title("➕ Add Expense")
 
-    step = st.number_input("Step")
+    with st.form("expense_form"):
 
-    transaction_type = st.selectbox(
-        "Transaction Type",
-        [
-            "PAYMENT",
-            "TRANSFER",
-            "DEBIT",
-            "CASH_OUT"
-        ]
-    )
-
-    amount = st.number_input("Amount")
-
-    sender = st.text_input("Sender")
-
-    receiver = st.text_input("Receiver")
-
-    if st.button("Add Transaction"):
-
-        conn = sqlite3.connect(
-            "database/finance.db"
+        expense_type = st.selectbox(
+            "Expense Type",
+            [
+                "PAYMENT",
+                "TRANSFER",
+                "CASH_OUT",
+                "DEBIT"
+            ]
         )
 
-        cursor = conn.cursor()
-
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS transactions (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                step INTEGER,
-                type TEXT,
-                amount REAL,
-                sender TEXT,
-                receiver TEXT
-            )
-        """)
-
-        cursor.execute("""
-            INSERT INTO transactions
-            (
-                step,
-                type,
-                amount,
-                sender,
-                receiver
-            )
-            VALUES (?, ?, ?, ?, ?)
-        """, (
-            step,
-            transaction_type,
-            amount,
-            sender,
-            receiver
-        ))
-
-        conn.commit()
-
-        conn.close()
-
-        st.success(
-            "Transaction added successfully!"
+        amount = st.number_input(
+            "Amount",
+            min_value=0.0,
+            step=100.0
         )
+
+        sender = st.text_input("Sender Name")
+
+        receiver = st.text_input("Receiver Name")
+
+        submit = st.form_submit_button("Add Expense")
+
+        if submit:
+
+            st.success("Expense Added Successfully!")

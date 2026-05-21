c47be2fbbf6a89ea55f803349882361dadf87625
ManojKS457 <manojdab10@gmail.com>
2026-05-21 22:46:16 +0530

    Update analytics.py

--- modules/analytics.py
+++ modules/analytics.py
@@ -4,50 +4,86 @@ import plotly.express as px
 
 def show_analytics():
 
-    st.title("📊 Expense Analytics")
+    st.title("📊 Financial Analytics")
 
-    df = pd.read_csv("data/sample_expense_data.csv")
+    df = pd.read_csv(
+        "data/sample_expense_data.csv"
+    )
 
     st.subheader("Dataset Preview")
 
     st.dataframe(df.head())
 
-    # CATEGORY ANALYSIS
+    # ---------------- PIE CHART ---------------- #
 
-    category_data = (
+    st.subheader("Transaction Types")
+
+    type_data = (
         df.groupby("type")["amount"]
         .sum()
         .reset_index()
     )
 
     fig1 = px.pie(
-        category_data,
+        type_data,
         names="type",
         values="amount",
-        title="Expense Distribution"
+        title="Transaction Distribution"
     )
 
-    st.plotly_chart(fig1, use_container_width=True)
+    st.plotly_chart(
+        fig1,
+        use_container_width=True
+    )
 
-    # BAR CHART
+    # ---------------- BAR CHART ---------------- #
+
+    st.subheader("Top Transactions")
+
+    top_data = (
+        df.sort_values(
+            by="amount",
+            ascending=False
+        )
+        .head(15)
+    )
 
     fig2 = px.bar(
-        df.head(20),
-        x="step",
+        top_data,
+        x="nameOrig",
         y="amount",
         color="type",
-        title="Transaction Amount Analysis"
+        title="Highest Transactions"
     )
 
-    st.plotly_chart(fig2, use_container_width=True)
-
-    # LINE CHART
-
-    fig3 = px.line(
-        df.head(50),
-        x="step",
-        y="amount",
-        title="Expense Trend"
+    st.plotly_chart(
+        fig2,
+        use_container_width=True
     )
 
-    st.plotly_chart(fig3, use_container_width=True)
+    # ---------------- FRAUD ANALYSIS ---------------- #
+
+    st.subheader("Fraud Transactions")
+
+    fraud_data = (
+        df["isFraud"]
+        .value_counts()
+        .reset_index()
+    )
+
+    fraud_data.columns = [
+        "Fraud",
+        "Count"
+    ]
+
+    fig3 = px.bar(
+        fraud_data,
+        x="Fraud",
+        y="Count",
+        title="Fraud Detection Analysis"
+    )
+
+    st.plotly_chart(
+        fig3,
+        use_container_width=True
+    )
